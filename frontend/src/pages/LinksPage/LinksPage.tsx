ad123a2a4644d8e7e029a83068789064c4a30ab6
Starfury-gh <vladimirel08@mail.ru>
2026-04-24 13:51:10 +0300

    frontend: add Links Page

---
+++ frontend/src/pages/LinksPage/LinksPage.tsx
@@ -0,0 +1,41 @@
+import { useState, useEffect } from "react";
+
+import LinkCard from "../../components/LinkCard";
+
+import { SH_API_URL } from "../../constants";
+import styles from "./LinksPage.module.css";
+
+interface LinkInfo {
+  slug: string;
+  original_url: string;
+}
+
+function LinkSstatsPage() {
+  const [links, setLinks] = useState<Array<LinkInfo>>([]);
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try {
+        const url = SH_API_URL + "/sh/";
+        const response = await fetch(url);
+        if (response.ok) {
+          const data = await response.json();
+          setLinks(data.links);
+        }
+      } catch (e) {
+        console.error("Error:", e);
+      }
+    };
+    fetchLinks();
+  }, []);
+  return (
+    <div className={styles["container"]}>
+      <div className={styles["links"]}>
+        {links.map((link) => (
+          <LinkCard original_url={link.original_url} slug={link.slug} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default LinkSstatsPage;
