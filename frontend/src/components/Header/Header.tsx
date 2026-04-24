7dd976ba74524ce5cffe8d163f7b9530a56342b1
Starfury-gh <vladimirel08@mail.ru>
2026-04-24 13:49:42 +0300

    frontend: add header

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,5 +1,21 @@
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+
+import styles from "./Header.module.css";
+
 function Header() {
-  return null;
+  return (
+    <header>
+      <div className={styles["links"]}>
+        <Button type="link" size="large">
+          <Link to="/">Shortify</Link>
+        </Button>
+        <Button type="link" size="large">
+          <Link to="/analytics">Analytics</Link>
+        </Button>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
