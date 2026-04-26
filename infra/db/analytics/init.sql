31c533c98b2a3a3400ea59ce4fa9a223ae2e8ccc
Starfury-gh <vladimirel08@mail.ru>
2026-04-26 13:29:11 +0300

    infra: db: analytics: add unique fields

--- infra/db/analytics/init.sql
+++ infra/db/analytics/init.sql
@@ -6,21 +6,24 @@ CREATE TABLE clicks (
 
 -- Интернет браузеры пользователей
 CREATE TABLE users_agents (
-    slug VARCHAR(12) REFERENCES clicks(slug) UNIQUE,
+    slug VARCHAR(12) REFERENCES clicks(slug),
     browser VARCHAR(32),
-    clicks_count INT
+    clicks_count INT,
+    raw_stat VARCHAR (255) UNIQUE
 );
 
 -- ОС пользователей
 CREATE TABLE users_os (
-    slug VARCHAR(12) REFERENCES clicks(slug) UNIQUE,
+    slug VARCHAR(12) REFERENCES clicks(slug),
     os VARCHAR(32),
-    clicks_count INT
+    clicks_count INT,
+    raw_stat VARCHAR (255) UNIQUE
 );
 
 -- Типы устройства пользователей
 CREATE TABLE users_devices (
-    slug VARCHAR(12) REFERENCES clicks(slug) UNIQUE,
+    slug VARCHAR(12) REFERENCES clicks(slug),
     device_type VARCHAR(32),
-    clicks_count INT
+    clicks_count INT,
+    raw_stat VARCHAR (255) UNIQUE
 );
