937212daf3266f96f1a6bcd916b833efd3ba18d4
Starfury-gh <vladimirel08@mail.ru>
2026-05-09 16:02:34 +0300

    infra: db: changed db schema. Users and shortener is same DB service

--- infra/db/shortener/init.sql
+++ infra/db/shortener/init.sql
@@ -1,5 +1,13 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE,
+    password VARCHAR(255),
+    created_at TIMESTAMP DEFAULT NOW()
+);
+
 CREATE TABLE short_urls (
     slug VARCHAR(12) PRIMARY KEY,
     origin VARCHAR(400),
-    created_at TIMESTAMP DEFAULT NOW()
+    created_at TIMESTAMP DEFAULT NOW(),
+    author_id INT REFERENCES users(id)
 );
