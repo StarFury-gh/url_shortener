0f3e2be553ec2e5d664cf3ad2c40bbd865bc5231
Starfury-gh <vladimirel08@mail.ru>
2026-05-16 14:41:47 +0300

    frontend: add config provider for antd

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,25 +7,39 @@ import {
   LinkAnalyticPage,
   LoginPage,
   RegisterPage,
+  ProfilePage,
 } from "./pages";
 import Header from "./components/Header";
+import { ConfigProvider } from "antd";
 
 function App() {
   const auth = useAuth();
   return (
-    <Router>
-      <Header auth={auth} />
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route
-          path="/analytics"
-          element={<LinksPage auth={auth.auth} userId={auth.user?.id} />}
-        ></Route>
-        <Route path="/analytics/:slug" element={<LinkAnalyticPage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
-      </Routes>
-    </Router>
+    <ConfigProvider
+      theme={{
+        token: {
+          fontSize: 16,
+        },
+      }}
+    >
+      <Router>
+        <Header auth={auth} />
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route
+            path="/analytics"
+            element={<LinksPage auth={auth.auth} userId={auth.user?.id} />}
+          ></Route>
+          <Route path="/analytics/:slug" element={<LinkAnalyticPage />}></Route>
+          <Route path="/login" element={<LoginPage />}></Route>
+          <Route path="/register" element={<RegisterPage />}></Route>
+          <Route
+            path="/profile"
+            element={<ProfilePage user={auth.user} auth={auth.auth} />}
+          ></Route>
+        </Routes>
+      </Router>
+    </ConfigProvider>
   );
 }
 
